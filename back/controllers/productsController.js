020d8c604edcc238d107a80430da8757b2d6681b
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-10-12 08:04:04 -0500

    CRUD productos

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,9 +1,32 @@
 const producto = require('../models/products');
+const fetch = (url) => import('node-fetch').then(({ default: fetch }) => fetch(url));
 
-exports.getProducts = (req, res, next) => {
+//ver la lista de productos
+exports.getProducts = async(req, res, next) => {
+
+    const productos = await producto.find();
+
     res.status(200).json({
         success: true,
-        message: "En esta ruta ud va a poder ver todos los productos"
+        count: productos.length,
+        productos
+    });
+}
+
+//buscar producto by id
+exports.getProductById = async (req, res, next) => {
+    
+    const productoById = await producto.findById(req.params.id);
+    if (!productoById) {
+        return res.status(404).json({
+            success: false,
+            message: `Producto con id ${req.params.id} no existe`
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        productoById
     })
 }
 
@@ -14,5 +37,64 @@ exports.newProduct = async (req, res, next) => {
     res.status(201).json({
         success: true,
         product
-    })
+    });
 }
+
+//update un producto
+exports.updateProduct = async (req, res, next) => {
+    let productoById = await producto.findById(req.params.id);
+    if (!productoById) {
+        return res.status(404).json({
+            success: false,
+            message: `Producto con id ${req.params.id} no existe`
+        });
+    }
+
+    productoById = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    
+    res.status(200).json({
+        success: true,
+        message: 'Producto actualizado correctamente',
+        productoById
+    });
+}
+
+//eliminar un producto
+exports.deleteProduct = async (req, res, next) => {
+    const productoById = await producto.findById(req.params.id);
+    if (!productoById) {
+        return res.status(404).json({
+            success: false,
+            message: `Producto con id ${req.params.id} no existe`
+        });
+    }
+
+    await productoById.remove();
+    
+    res.status(200).json({
+        success: true,
+        message: 'Producto eliminado correctamente',
+    });
+}
+
+//fetch
+function verProductos() {
+    fetch('http://localhost:4000/api/productos')
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
+
+// verProductos();
+
+function verProductoPorId(id) {
+    fetch('http://localhost:4000/api/producto/' + id)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
+
+// verProductoPorId('6346a5fc7bc30d30a7c65598');
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productsController');
+const { getProducts, newProduct, getProductById, updateProduct, deleteProduct } = require('../controllers/productsController');
 
 router.route('/productos').get(getProducts);
 
 router.route('/producto/nuevo').post(newProduct);
+
+router.route('/producto/:id').get(getProductById);
+
+router.route('/producto/:id').put(updateProduct);
+
+router.route('/producto/:id').delete(deleteProduct);
 
 module.exports = router;
