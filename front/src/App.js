fbbfbc0dd7ad444490a68f93653ca9255f451b7b
Juan Diego Salazar Velasquez <salazar.jd@gmail.com>
2022-10-13 09:47:22 -0500

    iniciando front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de mascotas Pet Shop</h1>
+      <img src="./images/logo.png" alt='logo petshop'></img>
+    </div>
+  );
+}
+
+export default App;
